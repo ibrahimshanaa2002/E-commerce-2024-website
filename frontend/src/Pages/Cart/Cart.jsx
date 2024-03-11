dbe8f7faba5f36d3b42d825f0f700acf8092b627
IbrahimShanaa <ibrahim.shanaa122@gmail.com>
2024-03-11 23:49:01 +0200

    CART

--- frontend/src/Pages/Cart/Cart.jsx
+++ frontend/src/Pages/Cart/Cart.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import "./Cart.css";
 import image from "../../assets/Brands/tshirt.jpg";
 import { FaTrashCan } from "react-icons/fa6";
+import { MdOutlineDiscount } from "react-icons/md";
+import { FaArrowRight } from "react-icons/fa6";
 
 const Cart = () => {
   return (
     <div>
-      <div className="title px-5">
-        <h1>Your Cart</h1>
+      <div className="title px-5 py-5">
+        <h1 className="text-5xl font-bold">Your Cart</h1>
       </div>
-      <div className="full-container  w-full flex px-5 gap-5">
-        <div className="left-side border-2 overflow-y-auto border-gray-400 rounded-2xl w-1/2 h-screen flex flex-row">
-          <div>
+      <div className="full-container  w-full flex px-5 py-5 gap-5">
+        <div className="left-side border-[1px] overflow-y-auto border-gray-300 rounded-2xl w-[70%] h-[50vh] flex flex-row ">
+          <div className="w-full">
             <div className="md:flex items-center p-5 border-t border-gray-200">
-              <div className="w-1/4 ">
+              <div className="w-[17%] ">
                 <img
                   src={image}
                   alt
@@ -49,7 +51,7 @@ const Cart = () => {
               </div>
             </div>
             <div className="md:flex items-center p-5 border-t border-gray-200">
-              <div className="w-1/4 ">
+              <div className="w-[17%] ">
                 <img
                   src={image}
                   alt
@@ -85,7 +87,7 @@ const Cart = () => {
               </div>
             </div>
             <div className="md:flex items-center p-5 border-t border-gray-200">
-              <div className="w-1/4 ">
+              <div className="w-[17%] ">
                 <img
                   src={image}
                   alt
@@ -121,7 +123,7 @@ const Cart = () => {
               </div>
             </div>
             <div className="md:flex items-center p-5 border-t border-gray-200">
-              <div className="w-1/4 ">
+              <div className="w-[17%] ">
                 <img
                   src={image}
                   alt
@@ -158,7 +160,56 @@ const Cart = () => {
             </div>
           </div>
         </div>
-        <div className="right-side border-2 border-gray-400 rounded-2xl w-1/2"></div>
+
+        <div className="right-side border-[1px] border-gray-300 rounded-2xl w-1/2 p-5">
+          <div>
+            <h1 className="text-3xl font-semibold pb-5">Order Summary</h1>
+
+            <div className="subtotal flex items-center justify-between py-2">
+              <h1 className="text-xl text-gray-700">Subtotal</h1>
+              <h2 className="text-xl font-bold">$565</h2>
+            </div>
+            <div className="discount flex items-center justify-between py-2">
+              <h1 className="text-xl text-gray-700">Discount (-20%)</h1>
+              <h2 className="text-xl text-red-500 font-bold">-$113</h2>
+            </div>
+            <div className="fees flex items-center justify-between py-2">
+              <h1 className="text-xl text-gray-700">Delivery Fee</h1>
+              <h2 className="text-xl  font-bold">$15</h2>
+            </div>
+            <hr />
+            <div className="total flex items-center justify-between py-2">
+              <h1 className="text-xl text-gray-700">Total</h1>
+              <h2 className="text-2xl  font-bold">$467</h2>
+            </div>
+            <div className="promo flex items-center gap-2">
+              <div class="relative w-[70%] py-5">
+                <input
+                  type="text"
+                  class="pl-10 pr-4 py-2 border rounded-3xl w-full bg-[#F0F0F0]"
+                  placeholder="Enter your email"
+                />
+                <div
+                  class="absolute inset-y-0 left-0 pl-3  
+                    flex items-center  
+                    pointer-events-none"
+                >
+                  <MdOutlineDiscount />
+                </div>
+              </div>
+              <div className="w-[30%] flex items-center justify-center bg-black text-white py-2 rounded-3xl cursor-pointer hover:bg-orange-500 duration-300 font-semibold">
+                <span>Apply</span>
+              </div>
+            </div>
+            <div className="checkout flex items-center justify-center bg-black text-white py-2 rounded-3xl cursor-pointer hover:bg-orange-500 duration-300 font-semibold gap-2">
+              <span>Go to Checkout</span>
+              <FaArrowRight />
+            </div>
+          </div>
+          <div className="logo h-[10vh]  flex justify-center items-center text-5xl font-extrabold uppercase">
+            <h1>Shop.co</h1>
+          </div>
+        </div>
       </div>
     </div>
   );
