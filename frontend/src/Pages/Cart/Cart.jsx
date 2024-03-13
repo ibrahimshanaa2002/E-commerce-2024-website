b9bed6d7c8903ab09e0d078cadfd0d56f4b95c1d
IbrahimShanaa <ibrahim.shanaa122@gmail.com>
2024-03-13 21:38:46 +0200

    res

--- frontend/src/Pages/Cart/Cart.jsx
+++ frontend/src/Pages/Cart/Cart.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import "./Cart.css";
-import { FaArrowRight, FaTrashCan } from "react-icons/fa6";
+import { FaTrashCan } from "react-icons/fa6";
 import { MdOutlineDiscount } from "react-icons/md";
 import { UserContext } from "../../context/userContext/userContextProvider";
 import axios from "axios";
@@ -41,11 +41,12 @@ const Cart = () => {
 
   // Calculate subtotal for all products in the cart
   const subtotal = productsInCart.reduce(
-    (total, product) => total + calculateTotalCost(product.quantity, product.newprice),
+    (total, product) =>
+      total + calculateTotalCost(product.quantity, product.newprice),
     0
   );
 
-  const TOOTAL=subtotal - (subtotal *DISCOUNT_RATE) +DELEVARY;
+  const TOOTAL = subtotal - subtotal * DISCOUNT_RATE + DELEVARY;
 
   return (
     <div>
@@ -59,59 +60,65 @@ const Cart = () => {
         )}
       </div>
       <div className="full-container w-full flex px-5 py-5 gap-5">
-        <div className="left-side border-[1px] overflow-y-auto border-gray-300 rounded-2xl w-[70%] h-[50vh] flex flex-row ">
+        <div className="left-sidess border-[1px] overflow-y-auto border-gray-300 rounded-2xl flex flex-row ">
           <div className="w-full">
             {productsInCart.map((product) => (
-              <div
-                key={product._id}
-                className="md:flex items-center p-5 border-t border-gray-200"
-              >
-                <div className="w-[17%]">
-                  <img
-                    src={product.img}
-                    alt=""
-                    className="w-full h-full object-center object-cover rounded-xl"
-                  />
-                </div>
-                <div className="md:pl-3 w-full">
-                  <div className="flex items-center justify-between w-full pt-1">
-                    <div className="product-title flex justify-between w-full">
-                      <p className="text-base font-black leading-none text-gray-800 pb-4">
-                        {product.title}
-                      </p>
-                      <FaTrashCan className="hover:text-red-600 duration-200 cursor-pointer" />
+              <div key={product._id} className="md:flex items-center p-5 ">
+                <div className="flex items-center">
+                  <div className="w-[17%] h-full">
+                    <div className="h-full">
+                      <img
+                        src={product.img}
+                        alt=""
+                        className="h-full w-full object-cover"
+                      />
                     </div>
                   </div>
-                  <p className="text-xs leading-3 text-gray-600 py-2 pt-3">
-                    Size: {product.size}
-                  </p>
-                  <p className="text-xs leading-3 text-gray-600 py-2 pb-3">
-                    Color: {product.color}
-                  </p>
-                  <div className="flex flex-col  justify-between pr-6">
-                    <div className="price flex items-center pt-3">
-                      <h1 className="mr-2 text-base font-black leading-none text-gray-800">
-                        Price:
-                      </h1>
-                      <p className="text-base font-black leading-none text-gray-800">
-                        ${product.newprice}
-                      </p>
+
+                  <div className="pl-3 w-full">
+                    <div className="flex items-center justify-between w-full pt-1">
+                      <div className="product-title flex justify-between w-full">
+                        <p className=" font-extrabold leading-none  ">
+                          {product.title}
+                        </p>
+                        <FaTrashCan className="hover:text-red-600 duration-200 cursor-pointer" />
+                      </div>
                     </div>
-                    <div className="price flex items-center pt-3">
-                      <h1 className="mr-2 text-base font-black leading-none text-gray-800">
-                        Quantity:
-                      </h1>
-                      <p className="text-base font-black leading-none text-gray-800">
-                        {product.quantity}
-                      </p>
-                    </div>
-                    <div className="price flex items-center pt-3">
-                      <h1 className="mr-2 text-base font-black leading-none text-gray-800">
-                        Total Cost:
-                      </h1>
-                      <p className="text-base font-black leading-none text-gray-800">
-                        ${calculateTotalCost(product.quantity, product.newprice)}
-                      </p>
+                    <p className="text-xs leading-3 text-gray-600  pt-3">
+                      Size: {product.size}
+                    </p>
+                    <p className="text-xs leading-3 text-gray-600 py-2 pb-3">
+                      Color: {product.color}
+                    </p>
+                    <div className="flex flex-col  justify-between  ">
+                      <div className="price flex items-center py-1">
+                        <h1 className="mr-2 text-base font-bold leading-none ">
+                          Price:
+                        </h1>
+                        <p className="  leading-none  text-gray-800">
+                          ${product.newprice}
+                        </p>
+                      </div>
+                      <div className="price flex items-center py-1">
+                        <h1 className="mr-2 text-base font-bold  leading-none ">
+                          Quantity:
+                        </h1>
+                        <p className=" leading-none text-gray-800">
+                          {product.quantity}
+                        </p>
+                      </div>
+                      <div className="price flex items-center py-1">
+                        <h1 className="mr-2 text-base font-bold  leading-none ">
+                          Total Cost:
+                        </h1>
+                        <p className="leading-none text-gray-800">
+                          $
+                          {calculateTotalCost(
+                            product.quantity,
+                            product.newprice
+                          )}
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -119,7 +126,7 @@ const Cart = () => {
             ))}
           </div>
         </div>
-        <div className="right-side border-[1px] border-gray-300 rounded-2xl w-1/2 p-5">
+        <div className="right-sidess border-[1px] border-gray-300 rounded-2xl w-1/2 p-5">
           <div>
             <h1 className="text-3xl font-semibold pb-5">Order Summary</h1>
             <div className="subtotal flex items-center justify-between py-2">
@@ -128,7 +135,9 @@ const Cart = () => {
             </div>
             <div className="discount flex items-center justify-between py-2">
               <h1 className="text-xl text-gray-700">Discount (-20%)</h1>
-              <h2 className="text-xl text-red-500 font-bold">${subtotal *DISCOUNT_RATE}</h2>
+              <h2 className="text-xl text-red-500 font-bold">
+                ${(subtotal * DISCOUNT_RATE).toFixed(2)}
+              </h2>
             </div>
             <div className="fees flex items-center justify-between py-2">
               <h1 className="text-xl text-gray-700">Delivery Fee</h1>
@@ -159,7 +168,7 @@ const Cart = () => {
               </div>
             </div>
           </div>
-          
+
           <div className="logo h-[10vh]  flex justify-center items-center text-5xl font-extrabold uppercase">
             <h1>Shop.co</h1>
           </div>
